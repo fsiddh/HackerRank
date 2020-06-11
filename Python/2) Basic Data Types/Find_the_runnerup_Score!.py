302503bc859364f1c0d52bb6f92293dd6ca6b0f9
Falansh Siddh <56497318+fsiddh@users.noreply.github.com>
2020-06-11 18:37:44 +0530

    Add files via upload

--- Python/2) Basic Data Types/Find_the_runnerup_Score!.py
+++ Python/2) Basic Data Types/Find_the_runnerup_Score!.py
@@ -1,12 +1,31 @@
-n = int(input())
-if 2 <= n <= 10:
-    pass
-l = list(map(int, input().split()))
-for i in l:
-    if -100 <= i <= 100:
+# Method 1:
+
+    n = int(input())
+    if 2 <= n <= 10:
         pass
-l.sort()
-l_s = set(l)
-l1 = list(l_s)
-updated_list = sorted(l1)
-print(updated_list[-2], end="")
+    l = list(map(int, input().split()))
+    for i in l:
+        if -100 <= i <= 100:
+            pass
+    l.sort()
+    l_s = set(l)
+    l1 = list(l_s)
+    updated_list = sorted(l1)
+    print(updated_list[-2], end="")
+
+# Method 2:- 
+
+    scores_list = list()
+
+    score_no = int(input())
+    scores = input().split()
+
+    for i in scores:
+        scores_list.append(int(i))
+
+    scores_list.sort()
+    max_score = max(scores_list)
+    index_max_score = scores_list.index(max_score)
+
+    print(scores_list[index_max_score-1])
+
